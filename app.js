be35ca3e0a3b8c258b9a22a0a280182f7f923c81
André ponce <andre_epr@outlook.com>
2020-11-29 01:10:59 -0300

    footer and content desktop

--- app.js
+++ app.js
@@ -20,3 +20,33 @@ function updateMargin(index) {
   let newMargin = index * document.body.clientWidth;
   document.querySelector(".carrousel-container").style.marginLeft = `-${newMargin}px`;
 }
+
+//-----------------------------------------------
+
+window.addEventListener('scroll', function() {
+  const underNav = document.querySelector('.under-nav')
+  const topNav = document.querySelector('.nav-desktop')
+
+  underNav.classList.toggle('sticky' , window.scrollY > 0)
+  topNav.classList.toggle('sticky-top' , window.scrollY > 0)
+})
+
+//--------------------------------------------
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.querySelector('.under-nav')
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
